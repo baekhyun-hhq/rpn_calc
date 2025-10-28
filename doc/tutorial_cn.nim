dfb1fa7c1c8fc1e4cf5ba1c9f5082518f392d11e
baekhyun-hhq <kyoong1012@gmail.com>
2025-10-28 11:12:24 +0800

    更新完整的RPN计算器使用文档

--- doc/tutorial_cn.nim
+++ doc/tutorial_cn.nim
@@ -3,7 +3,19 @@ import nimib
 nbInit
 nbText: """
 # RPN计算器使用手册
-测试文档
-"""
 
+## 安装和编译指南
+
+### 环境要求
+- Ubuntu Linux
+- GCC 编译器
+- Nim 语言环境 (用于生成本文档)
+
+### 编译步骤
+```bash
+cd ~/Work/rpn_calc
+g++ -std=c++11 rpn_calc.cpp -o rpn_calc
+
+./rpn_calc
+"""
 nbSave
